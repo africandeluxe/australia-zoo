3c24a2dc8b7a426e4e1ae48bda6a021378828b23
Africandeluxe <miski.mohamedahmed@gmail.com>
2024-05-17 12:00:39 +0200

    Animal data and images for each animal

---
+++ src/data/animals.js
@@ -0,0 +1,103 @@
+const animals = [
+  {
+    name: 'Echidna',
+    lifespan: '50 years',
+    group: 'Mammals',
+    food: 'insects such as ants and termites, beetle larvae and worms',
+    description: 'Echidnas, also called spiny anteaters, are walking contradictions. They are mammals, but they lay eggs...',
+    length: '76cm',
+    weight: '10kg',
+    found: 'Throughout Australia',
+    image: '/public/animals/echidna.jpg'
+  },
+  {
+    name: 'Frill-necked Lizard',
+    lifespan: '20 years',
+    group: 'Reptile',
+    food: 'Small insects and spiders',
+    description: 'When this unique creature feels threatened, it rises on its hind legs, opens its yellow-coloured mouth...',
+    length: '90cm',
+    weight: '1 kg',
+    found: 'Northern Australia',
+    image: '/public/animals/frill-necked-lizard.jpg'
+  },
+  {
+    name: 'Cassowary',
+    lifespan: '20 years',
+    group: 'Bird',
+    food: 'Plants matter like fruit, insects and small animals like mice and lizards',
+    description: 'It’s not hard to imagine that cassowaries are descended from dinosaur ancestors...',
+    length: '1.7m',
+    weight: '44kg',
+    found: 'Queensland',
+    image: '/public/animals/cassowary.jpg'
+  },
+  {
+    name: 'Tasmanian Devil',
+    lifespan: '5 years',
+    group: 'Mammals',
+    food: 'A predator, then eat meat from other animals such as wallabies and wombats',
+    description: 'The Tasmanian devil (Sarcophilus harrisii) (palawa kani: purinina) is a carnivorous marsupial of the family Dasyuridae...',
+    length: '70cm',
+    weight: '10kg',
+    found: 'Tasmania',
+    image: '/public/animals/tasmanian-devil.jpg'
+  },
+  {
+    name: 'Hawksbill Turtle',
+    lifespan: '50 years',
+    group: 'Reptile',
+    food: 'other animals (sponges & jellyfish), sea plants',
+    description: 'The Hawksbill Sea Turtle gets its common name from the distinctive mouth, which resembles a bird’s beak...',
+    length: '80cm (carapace)',
+    weight: '50kg',
+    found: 'Tropical coasts of Queensland, Northern Territory and Western Australia',
+    image: '/public/animals/hawksbill-turtle.jpg'
+  },
+  {
+    name: 'Kookaburra',
+    lifespan: '20 years',
+    group: 'Bird',
+    food: 'Insects and smalll animals including snakes, frogs and lizards',
+    description: 'Native to the eucalyptus forests of eastern Australia, the laughing kookaburra is the largest member of the Kingfisher family...',
+    length: '43cm',
+    weight: '300g',
+    found: 'Australia wide',
+    image: '/public/animals/kookaburra.jpg'
+  },
+  {
+    name: 'Quokka',
+    lifespan: '10 years',
+    group: 'Mammals',
+    food: 'Plant eaters, they munch on shrubs and grasses',
+    description: 'The Quokka, Setonix brachyurus, was described by early Dutch explorer, Willem de Vlamingh, "as a kind of rat as big as a common cat"...',
+    length: '50cm',
+    weight: '3kg',
+    found: 'Only found on Rottnest Island and a few places on mainland Western Australia',
+    image: '/public/animals/quokka.jpg'
+  },
+  {
+    name: 'Perentie',
+    lifespan: '20 years',
+    group: 'Reptile',
+    food: 'Carnivore, they eat animals like kangaroos, rabbits, lizards and birds',
+    description: 'The perentie (Varanus giganteus) is the largest monitor lizard or goanna native to Australia...',
+    length: '2.5m',
+    weight: '20kg',
+    found: 'Deserts',
+    image: '/public/animals/perentie.jpg'
+  },
+  {
+    name: 'Yellow Tailed Black Cockatoo',
+    lifespan: '41 years',
+    group: 'Bird',
+    food: 'Fruit, seeds and other plant material',
+    description: 'It has a short crest on the top of its head. Its plumage is mostly brownish black and it has prominent yellow cheek patches...',
+    length: '65cm',
+    weight: '900g',
+    found: 'SE Australia',
+    image: '/public/animals/yellow-tailed-black-cockatoo.jpg'
+  }
+];
+
+export default animals;
