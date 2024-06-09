269dd2fea48ccfc419fc9ebecadef57e8d7564ce
Africandeluxe <miski.mohamedahmed@gmail.com>
2024-06-09 12:57:07 +0200

    Added hamburgar menu on smaller devices

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,12 @@ import { Link } from 'react-router-dom';
 import styles from '../Styles.module.css';
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <header className={styles.header}>
       <div className={styles.logoContainer}>
@@ -10,7 +16,10 @@ const Header = () => {
       </div>
       <div className={styles.title}>Australia Zoo
       </div>
-      <nav>
+      <div className={styles.hamburger} onClick={toggleMenu}>
+        &#9776;
+      </div>
+      <nav className={`${styles.nav} ${menuOpen ? styles.open : ''}`}>
         <ul>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/mammals">Mammals</Link></li>
